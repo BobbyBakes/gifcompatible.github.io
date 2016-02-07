c9920f5d9f2c7798fdd7e94639e28a1d90535703
gifcompatible <okeydopeness@gmail.com>
2016-02-07 15:48:47 -0600

    Add script for cheatsheet mouseover event

--- projects/cheatsheet.js
+++ projects/cheatsheet.js
@@ -1,4 +1,4 @@
-var link_to_populate = document.getElementsByClassName("cheatlink");
-link_to_populate.addEventListener("hover", function(){
-    document.getElementById("cheatframe").src=link_to_populate.src
-});
+$(".cheatlink").on('mouseover', function(e){
+  $("#cheatframe").attr("src", $(this).attr("href"));
+  $("#cheatframe").show();
+})
